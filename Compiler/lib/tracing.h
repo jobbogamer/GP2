f0346bd6d5784da1f42203706dd196fb55576832
Josh Asch <josh.asch@gmail.com>
2017-02-22 17:50:32 +0000

    🚧 Create tracing.h and tracing.c in the lib
    These will be used by the compiled program to perform tracing. Currently the c file is empty, but the .h contains all the necessary function declarations and a TracingContext enum used to generalise starting and ending XML tags.

---
+++ Compiler/lib/tracing.h
@@ -0,0 +1,79 @@
+/* ///////////////////////////////////////////////////////////////////////////
+
+  Copyright 2015-2016 Christopher Bak
+
+  This file is part of the GP 2 Compiler. The GP 2 Compiler is free software: 
+  you can redistribute it and/or modify it under the terms of the GNU General
+  Public License as published by the Free Software Foundation, either version 3
+  of the License, or (at your option) any later version.
+
+  The GP 2 Compiler is distributed in the hope that it will be useful, but 
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+  more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the GP 2 Compiler. If not, see <http://www.gnu.org/licenses/>.
+
+  ==============
+  Tracing Module
+  ==============
+                             
+  Contains functions for tracing the execution of a compiled GP2 program.
+
+/////////////////////////////////////////////////////////////////////////// */
+
+#ifndef INC_TRACING_H
+#define INC_TRACING_H
+
+
+#include "globals.h"
+#include "morphism.h"
+#include "graph.h"
+#include "label.h"
+
+
+typedef enum {
+  PROGRAM = 0,
+  BACKTRACKING,
+  RULE_CALL,
+  RULE_MATCH,
+  RULE_APPLICATION,
+  RULE_SET,
+  PROCEDURE,
+  LOOP,
+  LOOP_ITERATION,
+  IF_STATEMENT,
+  TRY_STATEMENT,
+  BRANCH_CONDITION,
+  BRANCH_THEN,
+  BRANCH_ELSE,
+  OR_STATEMENT,
+  OR_LEFT,
+  OR_RIGHT
+} TracingContext;
+
+
+void createTraceFile(char* program_name, char* host_graph_name);
+void finishTraceFile();
+
+void traceBeginContext(TracingContext context, char* name);
+void traceEndContext(TracingContext context);
+
+void traceRuleMatch(Morphism* match, bool failed);
+
+void traceDeletedEdge(Edge* edge);
+void traceDeletedNode(Node* node);
+void traceRemarkedEdge(Edge* edge, MarkType old_mark);
+void traceRemarkedEdge(Node* node, MarkType old_mark);
+void traceRelabelledEdge(Edge* edge, HostLabel old_label);
+void traceRelabelledNode(Node* node, HostLabel old_label);
+void traceCreatedEdge(Edge* edge);
+void traceCreatedNode(Node* node);
+void traceChangeRootNode(Node* old_root, Node* new_root);
+
+void traceBreak();
+void traceSkip();
+void traceFail();
+
+#endif /* #ifndef INC_TRACING_H */
