6bea5f88edc79d32bb9b67f47858be88ecc90cf7
Josh Asch <josh.asch@gmail.com>
2017-03-05 11:00:12 +0000

    🔧 Use a different program in the test script
    Use one that’s designed to test different structures and operatios

--- Compiler/src/test.sh
+++ Compiler/src/test.sh
@@ -14,10 +14,10 @@ fi
 
 # Navigate to the location of the test GP2 program. This means the program name in the trace
 # will not have the giant path at the start.
-pushd /Users/Josh/Dropbox/University/Project/GP2_Programs/Invalid2ColourProgram/graphs/ > /dev/null
+pushd /Users/Josh/Dropbox/University/Project/GP2_Programs/TracingTest/graphs/ > /dev/null
 
 # Compile the GP2 program.
-gp2compile -t ../programs/2colourable.gp2.tmp ./not_2_colourable.host
+gp2compile -t ../programs/apply.gp2.tmp ./input.host
 
 if [ $? -ne 0 ]; then
     exit 1
@@ -27,7 +27,7 @@ fi
 pushd /tmp/gp2/
 
 # Copy the input graph to the current directory so the program can find it.
-cp /Users/Josh/Dropbox/University/Project/GP2_Programs/Invalid2ColourProgram/graphs/not_2_colourable.host ./
+cp /Users/Josh/Dropbox/University/Project/GP2_Programs/TracingTest/graphs/input.host ./
 
 # Build the program.
 make
